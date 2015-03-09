fead2aa747df912d3d76352555c3c06043e736eb
robochris <robochrisc@gmail.com>
2015-03-08 20:22:01 -0400

    scroll to top on state change

--- www/app/map-menu/MapMenuController.js
+++ www/app/map-menu/MapMenuController.js
@@ -4,7 +4,7 @@
     window.agApp.controller('MapMenuController', Controller);
 
 
-    function Controller($scope, $mdSidenav, $stateParams, MapMenuService, MapListService) {
+    function Controller($scope, $mdSidenav, $stateParams, MapMenuService, MapListService, $window) {
         var vm = this;
         vm.mapId = $stateParams.mapId;
         vm.menuItems = [];
@@ -28,6 +28,7 @@
         }
         
         $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+            $window.scrollTo(0, 0);
             if (toState.name == 'map-menu') {
                 vm.menuOnly = true;
             }
