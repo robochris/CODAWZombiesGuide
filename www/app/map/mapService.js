adc6e4628956bb04f3d91b13ff106db1ddc06bdc
Travis <travistx@gmail.com>
2015-04-10 15:17:07 -0400

    feat(map): pinch to zoom

--- www/app/map/mapService.js
+++ www/app/map/mapService.js
@@ -12,19 +12,15 @@
 
         function GetMap(mapId) {
             if (mapId == 2) {
-                return [
-                    {
+                return {
                         mapimgId: 1,
                         imageUrl: 'img/mapOutbreak.png'
-                }
-            ]
+                };
             } else if (mapId == 1) {
-                return [
-                    {
+                return {
                         mapimgId: 2,
                         imageUrl: "http://img2.wikia.nocookie.net/__cb20150131092412/callofduty/images/thumb/d/d5/Riot_Map_Layout_AW.png/283px-Riot_Map_Layout_AW.png"
-                    }
-            ]
+                    };
             }
         }
     }
