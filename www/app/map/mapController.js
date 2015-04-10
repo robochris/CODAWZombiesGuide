7eb506b11e07fcc9b69f116d560f758d79202201
Travis <travis.collins@datalinksoftware.com>
2015-04-10 15:11:25 -0400

    feat(map): pinch to zoom

--- www/app/map/mapController.js
+++ www/app/map/mapController.js
@@ -4,7 +4,7 @@
     window.agApp.controller('MapController', Controller);
 
 
-    function Controller(MapService,$stateParams,$scope) {
+    function Controller(MapService,$stateParams,$scope, $timeout) {
         var vm = this;
         vm.map = [];
         vm.mapId = $stateParams.mapId;
@@ -13,6 +13,29 @@
         function Activate() {
             $scope.mapMenu.selectedScreen = 'Map';
             vm.map = MapService.GetMap(vm.mapId);
+            
+            
+            $timeout(function() {
+                var $panzoom = $('.panzoom')
+                .panzoom({
+                    startTransform: 'scale(1.3)'
+                })                
+                .panzoom('zoom', 1.0, {
+                    animate: true
+                });
+
+                $panzoom.parent().on('mousewheel.focal', function( e ) {
+                    e.preventDefault();
+                    var delta = e.delta || e.originalEvent.wheelDelta;
+                    var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
+                    $panzoom.panzoom('zoom', zoomOut, {
+                      increment: 0.1,
+                      animate: false,
+                      focal: e
+                    });
+                });
+            });
+            
         }
     }
 
